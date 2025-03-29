06816504dcd7f9f5b7e11f4c031fa7fbb57f66d0
Nafiladi <nafiladi9@gmail.com>
2025-03-29 14:15:58 +0000

    Encryted Code Update

---
+++ prevent-rc.js
@@ -0,0 +1,18 @@
+<script src="prevent-rc.js"></script>
+
+// Disable right-click
+document.addEventListener('contextmenu', (event) => event.preventDefault());
+
+// Disable F12 and other key combinations
+document.addEventListener('keydown', (event) => {
+    // Disable F12
+    if (event.key === 'F12') {
+        event.preventDefault();
+    }
+
+    // Disable Ctrl+Shift+I (Inspect), Ctrl+U (View Source), and Ctrl+Shift+J (Console)
+    if ((event.ctrlKey && event.shiftKey && (event.key === 'I' || event.key === 'J')) || 
+        (event.ctrlKey && event.key === 'U')) {
+        event.preventDefault();
+    }
+});
